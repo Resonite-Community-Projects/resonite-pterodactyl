29e2cf3ad2c7782c9fbf1933181e9eebe130e995
Gray <graycrofford@gmail.com>
2022-06-30 21:40:13 +0000

    undo

--- start_neosvr.sh
+++ start_neosvr.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ -z "$STY" ]; then exec screen -dm -S screenName /bin/bash "$0"; fi
-
-"${STEAMCMDDIR}/steamcmd.sh" \
-+force_install_dir ${STEAMAPPDIR} \
-+login ${STEAMLOGIN} \
-+app_license_request ${STEAMAPPID} \
-+app_update ${STEAMAPPID} -beta ${STEAMBETA} -betapassword ${STEAMBETAPASSWORD} validate \
-+quit
+bash "${STEAMCMDDIR}/steamcmd.sh" \
+	+force_install_dir ${STEAMAPPDIR} \
+	+login ${STEAMLOGIN} \
+	+app_license_request ${STEAMAPPID} \
+	+app_update ${STEAMAPPID} -beta ${STEAMBETA} -betapassword ${STEAMBETAPASSWORD} validate \
+	+quit
 find ${STEAMAPPDIR}/Data/Assets -type f -atime +7 -delete
 find ${STEAMAPPDIR}/Data/Cache -type f -atime +7 -delete
 find /Logs -type f -name *.log -atime +30 -delete
