419793f0ed9bd9be399c22b65f9aa760ac6847bf
Gray <graycrofford@gmail.com>
2022-06-30 23:55:09 +0000

    don't detach

--- start_neosvr.sh
+++ start_neosvr.sh
@@ -9,5 +9,4 @@ bash "${STEAMCMDDIR}/steamcmd.sh" \
 find ${STEAMAPPDIR}/Data/Assets -type f -atime +7 -delete
 find ${STEAMAPPDIR}/Data/Cache -type f -atime +7 -delete
 find /Logs -type f -name *.log -atime +30 -delete
-screen -d -m bash -c 'mono ${STEAMAPPDIR}/Neos.exe -c /Config/Config.json -l /Logs'
-tail -f /dev/null
+screen -m bash -c 'mono ${STEAMAPPDIR}/Neos.exe -c /Config/Config.json -l /Logs'
