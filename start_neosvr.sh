429414ec8c849c91b31b882844470e8b6d940a67
Gray <graycrofford@gmail.com>
2022-06-30 21:56:50 +0000

    have to hold the shell open maybe

--- start_neosvr.sh
+++ start_neosvr.sh
@@ -10,3 +10,4 @@ find ${STEAMAPPDIR}/Data/Assets -type f -atime +7 -delete
 find ${STEAMAPPDIR}/Data/Cache -type f -atime +7 -delete
 find /Logs -type f -name *.log -atime +30 -delete
 screen -d -m -S "headless" -c 'mono ${STEAMAPPDIR}/Neos.exe -c /Config/Config.json -l /Logs'
+read -p "running"
