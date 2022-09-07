746a839a4dc206f17ab77306767ed1bcb6e851b4
Gray <graycrofford@gmail.com>
2022-09-07 23:51:04 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,6 @@
 
 cd Steam/steamapps/common/NeosVR
 
+jq --argjson roles "$DEFAULT_USER_ROLES" '.startWorlds[0].defaultUserRoles = $roles' Config/Config.json > Config/Config.json.tmp && mv Config/Config.json.tmp Config/Config.json
+
 ${STARTUP}
