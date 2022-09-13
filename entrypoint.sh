ae84729324514ca603cf00b57bdccc9e7ad209ce
Gray <graycrofford@gmail.com>
2022-09-13 17:43:24 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 cd Steam/steamapps/common/NeosVR
 
+#parse default user roles through jq, because pterodactyl's json parser breaks when replacing a nested array
 jq --argjson roles "$DEFAULT_USER_ROLES" '.startWorlds[0].defaultUserRoles = $roles' Config/Config.json > Config/Config.json.tmp && mv Config/Config.json.tmp Config/Config.json
 
 ${STARTUP}
