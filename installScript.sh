20a010ca00920249353684268fa116d561c5bc27
Gray <graycrofford@gmail.com>
2022-09-13 17:33:27 +0000

    Add new file

---
+++ installScript.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# steamcmd Base Installation Script
+#
+# Server Files: /mnt/server
+
+SRCDS_APPID=740250
+
+if [ "${STEAM_USER}" == "" ]; then
+    echo -e "steam user is not set.\n"
+    exit 1
+else
+    echo -e "user set to ${STEAM_USER}"
+fi
+
+cd /tmp
+mkdir -p /mnt/server/steamcmd
+curl -sSL -o steamcmd.tar.gz https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+tar -xzvf steamcmd.tar.gz -C /mnt/server/steamcmd
+cd /mnt/server/steamcmd
+
+chown -R root:root /mnt
+export HOME=/mnt/server
+
+./steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} +app_license_request ${STEAMAPPID} +app_update ${SRCDS_APPID} -beta ${STEAMBETA} -betapassword ${STEAMBETAPASSWORD} validate +quit
+
+cd ~/
+
+export STEAMAPPDIR=Steam/steamapps/common/NeosVR
+
+mkdir ${STEAMAPPDIR}/Config
+
+curl -sSL -o ${STEAMAPPDIR}/Config/Config.json "https://gitlab.com/BoltWolf/neosvr-headless/-/snippets/2403983/raw/main/Config.json"
+
+echo "done"
