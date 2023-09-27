05cfe86be8e82462121b6fabb13a733b440b42f0
Gray <graycrofford@gmail.com>
2023-09-27 19:49:28 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd Steam/steamapps/common/NeosVR
+cd Steam/steamapps/common/Resonite
 
 #parse default user roles through jq, because pterodactyl's json parser breaks when replacing a nested array. You should be using group cloud vars for this if you can, only use this for a personal headless server session.
 jq --argjson roles "$DEFAULT_USER_ROLES" '.startWorlds[0].defaultUserRoles = $roles' Config/Config.json > Config/Config.json.tmp && mv Config/Config.json.tmp Config/Config.json
