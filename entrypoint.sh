3ff96863456a7902ff83d9a42981f35a44d2f3ec
Gray <graycrofford@gmail.com>
2023-10-23 12:05:35 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+STEAMAPPID=2519830
+
+./steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +app_license_request ${STEAMAPPID} +app_update ${STEAMAPPID} -beta ${STEAMBETA} -betapassword ${STEAMBETAPASS} validate +quit
+
 cd Steam/steamapps/common/Resonite/Headless
 
 #parse default user roles through jq, because pterodactyl's json parser breaks when replacing a nested array. You should be using group cloud vars for this if you can, only use this for a personal headless server session.
