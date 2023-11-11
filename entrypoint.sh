903c7a9ade30d6370b624452eb7f09d9979a86ae
Gray <graycrofford@gmail.com>
2023-11-11 15:45:16 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,6 @@ STEAMAPPID=2519830
 
 ./steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +app_update ${STEAMAPPID} -beta ${STEAMBETA} -betapassword ${STEAMBETAPASS} validate +quit
 
-# hot patch for https://github.com/Yellow-Dog-Man/Resonite-Issues/issues/592
-cp Steam/steamapps/common/Resonite/Resonite_Data/Managed/TwitchLib.Communication.dll Steam/steamapps/common/Resonite/Headless/TwitchLib.Communication.dll
-
 cd Steam/steamapps/common/Resonite/Headless
 
 # parse default user roles through jq, because pterodactyl's json parser breaks when replacing a nested array. You should be using group cloud vars for this if you can, only use this for a personal headless server session.
